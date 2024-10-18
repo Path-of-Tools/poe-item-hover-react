5b4aba429b584165f81ae4d05e54f085b1aeee19
Petter Kaspersen <petter@kaspersen.dev>
2024-10-18 18:17:42 +0200

    Correctly place item

--- src/components/ItemHover.tsx
+++ src/components/ItemHover.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useItem } from "../hooks/useItem";
 import ItemSeparator from "./ItemSeparator";
 
@@ -16,8 +16,30 @@ export function ItemHover({ itemData }: ItemHoverProps) {
     ItemExplicits,
   } = useItem(itemData);
 
+  const itemRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (itemRef.current) {
+      const _item = itemRef.current;
+      const boundingBox = _item.getBoundingClientRect();
+
+      const rightX = boundingBox.x + boundingBox.width;
+      const bottomY = boundingBox.y + boundingBox.height;
+
+      if (rightX > window.innerWidth) {
+        _item.style.left = `calc(100% - ${boundingBox.width / 2 + 50}px)`;
+      }
+
+      if (bottomY > window.innerHeight) {
+        _item.style.top = `calc(100% - ${boundingBox.height}px)`;
+      }
+    }
+  }, [itemRef]);
+
   return (
-    <div className="absolute item-hover z-50 top-0 left-full bg-black/80 rounded-md  shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] font-poe">
+    <div
+        ref={itemRef} 
+        className="pointer-events-none absolute item-hover z-50 top-0 left-full bg-black/80 rounded-md  shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] font-poe">
       <div className="flex flex-col gap-2">
         {itemName.length > 0 ? (
           <div className="rare-header__double flex flex-col text-center items-center">
